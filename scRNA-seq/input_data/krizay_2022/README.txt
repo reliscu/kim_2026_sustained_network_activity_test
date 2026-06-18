50723594257cf9c36eec18a8396d9e4716e66496
reliscu <rebeliscu@gmail.com>
2026-06-18 14:44:15 -0700

    cleaning up

--- scRNA-seq/input_data/krizay_2022/README.txt
+++ scRNA-seq/input_data/krizay_2022/README.txt
@@ -1,1 +1,3 @@
-Krizay, D. K., Goldstein, D. B. & Boland, M. J. Single Cell transcriptional analysis of ex vivo models of cortical and hippocampal development identifies unique longitudinal trends. bioRxiv 2022.12.11.519959 (2022) doi:10.1101/2022.12.11.519959.
+Citation: Krizay, D. K., Goldstein, D. B. & Boland, M. J. Single Cell transcriptional analysis of ex vivo models of cortical and hippocampal development identifies unique longitudinal trends. bioRxiv 2022.12.11.519959 (2022) doi:10.1101/2022.12.11.519959.
+
+Data will be made publicly available after publication.
--- splicing/01_run_rmats.sh
+++ splicing/01_run_rmats.sh
@@ -3,46 +3,46 @@ cd splicing
 # get list of bams per condition
 
 ls input_data/bams/mCN11*bam input_data/bams/mCN12*bam | paste -sd "," > ctrl_bams.txt
-ls input_data/bams/mCN13*bam input_data/bams/mCN14*bam | paste -sd "," > caltrap_naive_bams.txt
-ls input_data/bams/mCN15*bam input_data/bams/mCN16*bam | paste -sd "," > caltrap_AR_bams.txt
+ls input_data/bams/mCN13*bam input_data/bams/mCN14*bam | paste -sd "," > caltrap_NA_bams.txt
+ls input_data/bams/mCN15*bam input_data/bams/mCN16*bam | paste -sd "," > caltrap_SR_bams.txt
 
 # run
 
 rmats_path=<path to rMATS binary>
 gtf=input_data/gencode.vM35.annotation.gtf # make sure this has been unzipped
 
-# ctrl vs. caltrap naive
+# ctrl vs. caltrap NA
 python "${rmats_path}/rmats.py" \
     --b1 ctrl_bams.txt \
-    --b2 caltrap_naive_bams.txt \
+    --b2 caltrap_NA_bams.txt \
     --gtf "$gtf" \
     -t paired \
     --variable-read-length \
     --readLength 100 \
     --nthread 20 \
-    --od data/rmats/ctrl_vs_naive \
-    --tmp data/rmats/ctrl_vs_naive
+    --od data/rmats/ctrl_vs_caltrap_NA \
+    --tmp data/rmats/ctrl_vs_caltrap_NA
 
-# ctrl vs. caltrap AR
+# ctrl vs. caltrap SR
 python "${rmats_path}/rmats.py" \
     --b1 ctrl_bams.txt \
-    --b2 caltrap_AR_bams.txt \
+    --b2 caltrap_SR_bams.txt \
     --gtf "$gtf" \
     -t paired \
     --variable-read-length \
     --readLength 100 \
     --nthread 20 \
-    --od data/rmats/ctrl_vs_AR \
-    --tmp data/rmats/ctrl_vs_AR
+    --od data/rmats/ctrl_vs_caltrap_SR \
+    --tmp data/rmats/ctrl_vs_caltrap_SR
 
-# caltrap naive vs. caltrap AR
+# caltrap NA vs. caltrap SR
 python "${rmats_path}/rmats.py" \
-    --b1 caltrap_naive_bams.txt \
-    --b2 caltrap_AR_bams.txt \
+    --b1 caltrap_NA_bams.txt \
+    --b2 caltrap_SR_bams.txt \
     --gtf "$gtf" \
     -t paired \
     --readLength 100 \
     --variable-read-length \
     --nthread 20 \
-    --od data/rmats/naive_vs_AR \
-    --tmp data/rmats/naive_vs_AR
+    --od data/rmats/caltrap_NA_vs_SR \
+    --tmp data/rmats/caltrap_NA_vs_SR
